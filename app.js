384653f8bb4be0578d80d61abfb43ca342108a69
Sachin Singh <90830959+Sachin79dev@users.noreply.github.com>
2026-05-30 14:06:25 +0530

    Create app.js
    Create app.js

---
+++ app.js
@@ -0,0 +1,25 @@
+// 40 Que series about array
+
+
+// 1.  Create an array of 5 favorite movies and print all values.
+
+
+let movies = ["Fight Club", "Resident Evil", "Avengers", "Venom", "3 idiot"];
+
+
+for (let i = 0; i < movies.length; i++) {
+    console.log(movies[i]);
+}
+
+
+
+let misle = [1, 2, "hello", true, false, [1, 2, 3, 4]]
+
+
+
+// 2. Create an array containing numbers, strings, boolean, and another array. Print only the nested array value.
+
+
+for (let j = 0; j < misle[5].length; j++) {
+    console.log(misle[5][j]);
+}
